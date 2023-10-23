f99cce1eab5ae0412efae367eda733bef6c36279
DogsTailFarmer <jerry.fedorenko@yahoo.com>
2023-10-23 22:18:00 +0300

    1.4.0b4  + fetch_ticker_price_change_statistics  + fetch_account_information  ...  + fetch_ledgers

--- crypto_ws_api/ws_session.py
+++ crypto_ws_api/ws_session.py
@@ -129,6 +129,7 @@ class UserWSS:
         res = await self.request('userDataStream.start', _api_key=True)
         if res is None:
             logger.warning(f"UserWSS: Not 'logged in' for {self.ws_id}")
+            raise ConnectionClosed
         else:
             if self.exchange == 'binance':
                 self._listen_key = res.get('listenKey')
